b06326878f2be7681e7c4d6019e0188a0c309bab
Vitor Ribeiro <vitorribeiro@MBP-de-Vitor.home>
2024-02-11 14:38:04 +0000

    feat: add logic to return a code using the trivial logic

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -19,6 +19,40 @@ routes.get('/grade', async (req, res) => {
         return matrix;
     }
 
+    function countLetterInMatrix(letter: string, matrix: string[][]): number {
+        let count = 0;
+        
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] === letter) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    function getCode(matrix: string[][]): string {
+        const date = new Date();
+        const seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+        const digits = seconds.toString().split('');
+        const coordinate = digits.map(Number);
+       
+        const firstCharacter = matrix[coordinate[0]][coordinate[1]];
+        const secondCharacter = matrix[coordinate[1]][coordinate[0]];
+
+        const occurrencesFirstChar = countLetterInMatrix(firstCharacter, matrix);
+        const occurrencesSecondChar = countLetterInMatrix(secondCharacter, matrix);
+
+        return occurrencesFirstChar.toString() + occurrencesSecondChar.toString();
+    }
+
     const randomAlphabetMatrix: string[][] = generateRandomAlphabetMatrix();
-    return res.status(201).send(randomAlphabetMatrix);
+    const code = getCode(randomAlphabetMatrix)
+
+    return res.status(201).send({
+        data: randomAlphabetMatrix,
+        code,
+    });
 })
