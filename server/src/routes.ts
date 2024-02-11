42bbf4d4d4339b287b1e8c4afda5940aa0243a26
Vitor Ribeiro <vitorribeiro@MBP-de-Vitor.home>
2024-02-11 13:39:00 +0000

    feat: create a endpoint to return a random alphabet matrix

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,23 @@ import express from 'express';
 
 export const routes = express.Router();
 
-routes.get('/', async (req, res) => {
-    return res.status(201).send();
+routes.get('/grade', async (req, res) => {
+    function generateRandomAlphabetMatrix(): string[][] {
+        const matrix: string[][] = [];
+        
+        for (let i = 0; i < 10; i++) {
+            const row: string[] = [];
+            for (let j = 0; j < 10; j++) {
+                const randomCharCode = Math.floor(Math.random() * 26) + 97; // Random char code from 'a' to 'z'
+                const randomLetter = String.fromCharCode(randomCharCode);
+                row.push(randomLetter);
+            }
+            matrix.push(row);
+        }
+        
+        return matrix;
+    }
+
+    const randomAlphabetMatrix: string[][] = generateRandomAlphabetMatrix();
+    return res.status(201).send(randomAlphabetMatrix);
 })
